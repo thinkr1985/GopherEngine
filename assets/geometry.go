22b53e56285b8c27cccf3d88a717d6a3499533e3
Nilesh <38728224+thinkr1985@users.noreply.github.com>
2025-08-02 17:35:41 +0530

    Update geometry and triangle structs
    Update geometry and triangle structs

--- assets/geometry.go
+++ assets/geometry.go
@@ -6,12 +6,16 @@ import (
 
 type Geometry struct {
 	Name      string
-	Transform nomath.Transform
+	Transform *nomath.Transform
+	Vertices  []*nomath.Vec3
+	Normals   []*nomath.Vec3
+	UVs       []*nomath.Vec2
+	Triangles []*Triangle
 }
 
 func (g *Geometry) NewGeometry() *Geometry {
 	return &Geometry{
 		Name:      "Object001",
-		Transform: *nomath.NewTransform(),
+		Transform: nomath.NewTransform(),
 	}
 }
---
+++ lookdev/materials.go
@@ -0,0 +1,23 @@
+package lookdev
+
+type Material struct {
+	Name                string
+	DiffuseColor        ColorRGBA
+	SpecularColor       ColorRGBA
+	Transparency        float64
+	Reflectivity        float64
+	DiffuseTexture      *Texture
+	SpecularTexture     *Texture
+	NormalTexture       *Texture
+	TransparencyTexture *Texture
+}
+
+func NewMaterial(name string) Material {
+	return Material{
+		Name:          name,
+		DiffuseColor:  ColorRGBA{R: 166, G: 166, B: 166, A: 1.0},
+		SpecularColor: ColorRGBA{R: 0, G: 0, B: 0, A: 1},
+		Transparency:  0.0,
+		Reflectivity:  0.0,
+	}
+}
